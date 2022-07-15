40640cc0badb8e3f6e9a08ee8dbd6833be52e4b6
KyungHyunLim <kyunghyun_lim@tmax.co.kr>
2022-07-15 15:48:54 +0900

    feature/add another displayer

--- src/monitoring_app/weatherstation.py
+++ src/monitoring_app/weatherstation.py
@@ -9,17 +9,23 @@ Author:
     Email: lkh1075@gmail.com
 """
 
-from monitoring_app.displays import CurrentConditionDisplay
+from monitoring_app.displays import (
+    CurrentConditionDisplay,
+    ForecastDisplay,
+    StatisticsDisplay,
+)
 from monitoring_app.weatherdata import WeatherData
 
 if __name__ == "__main__":
     weather_data = WeatherData()
 
     current_display = CurrentConditionDisplay(weather_data)
+    static_display = StatisticsDisplay(weather_data)
+    forecast_display = ForecastDisplay(weather_data)
 
     print("-" * 100)
     weather_data.set_measurements(80, 65, 59)
     print("기상변화", "-" * 90)
-    weather_data.set_measurements(70, 61, 87)
+    weather_data.set_measurements(45, 61, 87)
     print("기상변화", "-" * 90)
-    weather_data.set_measurements(75, 65, 92)
+    weather_data.set_measurements(111, 65, 92)
